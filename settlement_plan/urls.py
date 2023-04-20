0806fb40582f2d6de678c144cf9d4eb26dc605fa
igor.slivka <igor.slivka@iitubuntu.dev.iitrust.int>
2023-04-20 11:15:32 +0300

    add test

--- settlement_plan/urls.py
+++ settlement_plan/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, re_path
 
 from .views import RecordTypeInvestView, CalculateView, ListInvestmentPurposeView, \
-    ListAndCreateInvestmentPortfolioView #ListUserInfoView, ListAndCreateDocument
+    ListAndCreateInvestmentPortfolioView
 
 urlpatterns = [
-    path('investment_portfolio/', ListAndCreateInvestmentPortfolioView.as_view()),
-    re_path(r'^calculate/(?P<type_invest>[a-z_A-Z]+)/$', CalculateView.as_view()),
-    re_path(r'^investment_purpose/(?P<type_invest>[a-z_A-Z]+)/$', RecordTypeInvestView.as_view()),
-    path('investment_purpose/', ListInvestmentPurposeView.as_view()),
+    path('investment_portfolio/', ListAndCreateInvestmentPortfolioView.as_view(), name='investment_portfolio_api'),
+    re_path(r'^calculate/(?P<type_invest>[a-z_A-Z]+)/$', CalculateView.as_view(), name='calculate_type_invest_api'),
+    re_path(r'^investment_purpose/(?P<type_invest>[a-z_A-Z]+)/$', RecordTypeInvestView.as_view(),
+            name='investment_purpose_type_invest_record'),
+    path('investment_purpose/', ListInvestmentPurposeView.as_view(), name='investment_purpose_api'),
 
 
 ]
