af38d1b711a8a94e654877dd58c1a96ac6e298a3
igor.slivka <igor.slivka@iitubuntu.dev.iitrust.int>
2023-04-21 15:32:40 +0300

    add compare_2

--- settlement_plan/utils.py
+++ settlement_plan/utils.py
@@ -13,38 +13,11 @@ class Calculate:
 
     @staticmethod
     def calculate_investment_sum(data):
-        period_monthly_invest = data['period_monthly_invest']
-        age_goal_achievement = data['age_goal_achievement']
-        average_sum = data['average_sum']
-        sum_rent_month = data['sum_rent_month']
-
-        # monthly_return = (1 + average_sum) ** (1 / 12) - 1  # ежемесячная доходность
-        # months = (age_goal_achievement - period_monthly_invest) * 12  # количество месяцев, в течение которых необходимо инвестировать
-        # investment_amount = (sum_rent_month / 0.05) * ((1 + 0.05) ** months - 1) / (
-        #             (1 + monthly_return) ** months - 1)  # расчет суммы инвестиций
-        # return round(investment_amount, 2)  # возвращаем результат с округлением до 2 знаков после запятой
+        return 1
 
     @staticmethod
     def calculate_sum_rent(data):
-        sum_rent_month = data['sum_rent_month']
-        period_monthly_invest = data['period_monthly_invest']
-        age_goal_achievement = data['age_goal_achievement']
-        average_sum = data['average_sum']
-        # переводим годовую доходность и процентную ставку в месячные
-        monthly_return = ((1 + average_sum) ** (1 / 12)) - 1
-        monthly_interest_rate = 0.5 / 12
-
-        # рассчитываем сумму капитала, который мы накопим к возрасту age
-        num_months = (age_goal_achievement - (period_monthly_invest * 12))
-        total_capital = 0
-
-        for i in range(period_monthly_invest * 12):
-            total_capital += sum_rent_month
-        total_capital *= (1 + monthly_return)
-        if i % 12 == 0:
-            total_capital *= (1 + monthly_interest_rate)
-
-        return round(total_capital, 2)
+        return 2
 
     @staticmethod
     def calculate_age_rent(data: dict):
